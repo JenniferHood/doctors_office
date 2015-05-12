9aa94ad4995b51521fe848e6ec76bf4087e64939
Guest User <student@epicodus.com>
2015-05-12 14:26:56 -0700

    Add methods and specs for doctor, run rspec test and pass.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,4 +5,26 @@ class Doctor
     @specialty = attributes.fetch(:specialty)
     @name = attributes.fetch(:name)
   end
+
+  define_method(:save) do
+    doctor = DB.exec("INSERT INTO doctors (name) VALUES ('#{@name}') RETURNING id;")
+    @id = doctor.first().fetch('id').to_i()
+  end
+
+  define_singleton_method(:all) do
+    doctors = DB.exec("SELECT * FROM doctors;")
+    returned_doctors = []
+    doctors.each() do |doctor|
+      name = doctor.fetch("name")
+      specialty = doctor.fetch("specialty")
+      returned_doctors.push(Doctor.new({:name => name, :specialty => specialty}))
+    end
+    returned_doctors
+  end
+
+  define_method(:==) do |another_doctor|
+    doctor = self.specialty().eql?(another_doctor.specialty())
+    doctor_name = self.name().eql?(another_doctor.name())
+    doctor.&(doctor_name)
+  end
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -7,4 +7,27 @@ describe(Doctor) do
       expect(test_doctor.specialty()).to(eq("neurologist"))
     end
   end
+
+  describe(':save') do
+    it('saves doctors name in the database') do
+      test_doctor = Doctor.new(:name => "James", :specialty => nil)
+      test_doctor.save()
+      expect(Doctor.all()).to(eq([test_doctor]))
+    end
+  end
+
+  describe('.all') do
+    it('program will return all of the doctors names, in this case it will be empty') do
+      expect(Doctor.all()).to(eq([]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same doctor if they have the same name and list ID") do
+      doctor1 = Doctor.new({:name => "Andrew", :specialty => nil})
+      doctor2 = Doctor.new({:name => "Andrew", :specialty => nil})
+      expect(doctor1).to(eq(doctor2))
+    end
+  end
+
 end
